38db11115ef81e270081a5b0f9d0718d15fa2126
KhaetbekIT <khaetbek@internet.ru>
2026-04-09 16:43:11 +0500

    fix: удалено данные

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	logging: {
-		fetches: {
-			fullUrl: true,
-		},
-	},
-	output: "standalone",
-	transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
-
-	headers: async () => {
-		return [
-			{
-				source: "/(.*)",
-				headers: [
-					{
-						key: "X-Developed-By",
-						value: "khaetbek.dev",
-					},
-				],
-			},
-		];
-	},
-	experimental: {
-		optimizePackageImports: [
-			"@radix-ui/react-accordion",
-			"@radix-ui/react-dialog",
-			"@radix-ui/react-slot",
-			"class-variance-authority",
-			"lucide-react",
-			"embla-carousel-react",
-		]
-	}
+  reactCompiler: true,
+  output: "standalone"
 };
 
 export default nextConfig;
